f6bde500ce5e3a0fb953214cfc2886d0626b18d3
jachipe <jachipe@students.mtsd.k12.wi.us>
2024-10-03 19:29:30 -0500

    updated TeleOpTemp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTemp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTemp.java
@@ -64,6 +64,8 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
+//testing purposes
+
 @TeleOp(name="TeleOpTest", group="Linear OpMode")
 
 public class TeleOpTemp extends LinearOpMode {
