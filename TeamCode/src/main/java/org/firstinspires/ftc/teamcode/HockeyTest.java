3f433270429d44aedac1890606a3985a30f3e260
Team4116 <jachipe@students.mtsd.k12.wi.us>
2025-09-06 20:40:50 -0500

    fixing lift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HockeyTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HockeyTest.java
@@ -12,14 +12,22 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class HockeyTest extends LinearOpMode {
     private DcMotor left = null;
     private DcMotor right = null;
+    private Servo push = null;
+    private DcMotor in = null
     @Override
     public void runOpMode() {
         left = hardwareMap.get(DcMotor.class, "left");
         right = hardwareMap.get(DcMotor.class, "right");
+        push = hardwareMap.get(Servo.class, "push");
+        in = hardwareMap.get(DcMotor.class, "in");
+
         waitForStart();
         while (opModeIsActive()) {
-            right.setPower(0.5);
-            left.setPower(-0.5);
+            right.setPower(0.3);
+            left.setPower(-0.3);
+            push.setPosition(gamepad2.left_stick_y);
+            in.setPower(0.5);
+
         }
     }
 
